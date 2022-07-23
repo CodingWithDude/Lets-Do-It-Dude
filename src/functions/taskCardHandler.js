98f5f8690435da204addef145c02682109ce5378
CodingWithDude <codingwithdude@gmail.com>
2022-07-22 20:04:48 -0700

    Updated taskCardHandler and main.css

--- src/functions/taskCardHandler.js
+++ src/functions/taskCardHandler.js
@@ -1,20 +1,20 @@
 import project from "./project";
 import updateTaskContainer from "./updateTaskContainer";
 
-const taskContainer = document.getElementById("task-container");
-
-const taskCard = document.createElement("div");
-const title = document.createElement("p");
-
-const buttonContainer = document.createElement("div");
-const removeBtn = document.createElement("button");
-
 const newTaskCard = (task) => {
-  taskCard.classList.add("book-card");
+  const taskContainer = document.getElementById("task-container");
+
+  const taskCard = document.createElement("div");
+  const title = document.createElement("p");
+
+  const buttonContainer = document.createElement("div");
+  const removeBtn = document.createElement("button");
+
+  taskCard.classList.add("task-card");
   buttonContainer.classList.add("buttonContainer");
   removeBtn.classList.add("removeBtn");
 
-  title.textContent = `"${task.title}"`;
+  title.textContent = `${task.title}`;
   removeBtn.textContent = "Remove";
 
   removeBtn.onclick = removeTaskCard;
